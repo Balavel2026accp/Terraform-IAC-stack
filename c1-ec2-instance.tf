576d0ad7644bef3272e2ea71d2bb02fac6b3ee9c
Accp <accplifelaba@gmail.com>
2026-06-30 07:49:47 +0530

    Resource update

---
+++ c1-ec2-instance.tf
@@ -0,0 +1,10 @@
+####### resouce creation ec2-instance #####
+resource "aws_instance" "Lab1" {
+    ami = "ami-0011550b539717e2a"
+    instance_type = "t3.micro"
+    user_data = file("${path.module}/app1-install.sh")
+
+    tags = {
+      Name = "lab1-server"
+    }
+}
