bfdee33ce13513aa893c3df78650d19df380f8e1
Accp <accplifelaba@gmail.com>
2026-06-30 06:27:16 +0530

    Seesion 03

---
+++ 03-Terraform,setting,provider,resource/c1-terraform.tf
@@ -0,0 +1,11 @@
+##########################################################################
+# Terraform setting block
+terraform {
+  required_version = "~> 0.14"
+  required_providers {
+   aws ={
+    source  = "hashicorp/aws"
+    version = "~>6.0"
+   }
+  }
+}   
