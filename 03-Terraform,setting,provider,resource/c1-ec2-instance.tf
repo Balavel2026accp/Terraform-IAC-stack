bd5a2bfd90ca91a3f11417aeb492dcbd76342488
Accp <accplifelaba@gmail.com>
2026-07-02 10:08:04 +0530

    terraform.ftstate

--- 03-Terraform,setting,provider,resource/c1-ec2-instance.tf
+++ 03-Terraform,setting,provider,resource/c1-ec2-instance.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "Lab1" {
     ami = "ami-0011550b539717e2a"
     instance_type = "t3.micro"
     user_data = file("${path.module}/app1-install.sh")
+    key_name = "terraform-key"
 
     tags = {
       Name = "lab1-server"
     }
-}#  
+} 
